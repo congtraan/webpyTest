6277d4e97d0399cbe0ab039917a80c88e19a3f3f
Thuan Cong Tran <congtran@123.175.202.84.customer.cdi.no>
2020-03-16 21:09:52 +0100

    changed return

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ urls = (
 
 class index:
     def GET(self):
-<<<<<<< HEAD
-        return "Hello, name!"
-=======
-        return "Hello, world.  im on this make change branch!"
->>>>>>> makeChange
+    	return ("hello")
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
